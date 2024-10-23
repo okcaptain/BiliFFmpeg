bced935b7c0042db577a15bc9c7f74c855bdf56f
okcaptain <okjackcaptain@gmail.com>
2024-10-23 23:00:33 +0800

    update

--- libavformat/avs3dec.c
+++ libavformat/avs3dec.c
@@ -26,6 +26,39 @@
 #include "avformat.h"
 #include "rawdec.h"
 
+const uint8_t *avpriv_find_start_code(const uint8_t *av_restrict p,
+                                      const uint8_t *end,
+                                      uint32_t *av_restrict state)
+{
+    int i;
+
+    av_assert0(p <= end);
+    if (p >= end)
+        return end;
+
+    for (i = 0; i < 3; i++) {
+        uint32_t tmp = *state << 8;
+        *state = tmp + *(p++);
+        if (tmp == 0x100 || p == end)
+            return p;
+    }
+
+    while (p < end) {
+        if      (p[-1] > 1      ) p += 3;
+        else if (p[-2]          ) p += 2;
+        else if (p[-3]|(p[-1]-1)) p++;
+        else {
+            p++;
+            break;
+        }
+    }
+
+    p = FFMIN(p, end) - 4;
+    *state = AV_RB32(p);
+
+    return p + 4;
+}
+
 static int avs3video_probe(const AVProbeData *p)
 {
     const uint8_t *ptr = p->buf, *end = p->buf + p->buf_size;
