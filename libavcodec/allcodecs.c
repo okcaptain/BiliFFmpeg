059fbce16e5e61e7b9ef85889c3518d8a5125637
okcaptain <okjackcaptain@gmail.com>
2024-10-24 22:26:35 +0800

    lavc,doc: add libuavs3d video decoder wrapper

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -690,6 +690,7 @@ extern AVCodec ff_libspeex_encoder;
 extern AVCodec ff_libspeex_decoder;
 extern AVCodec ff_libtheora_encoder;
 extern AVCodec ff_libtwolame_encoder;
+extern AVCodec ff_libuavs3d_decoder;
 extern AVCodec ff_libvo_amrwbenc_encoder;
 extern AVCodec ff_libvorbis_encoder;
 extern AVCodec ff_libvorbis_decoder;
